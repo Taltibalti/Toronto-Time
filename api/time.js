b59461d68c592a2651622c1fca532bd0b7418360
Taltibalti <ahnafrakin123@gmail.com>
2026-03-18 01:56:40 -0400

    Update time.js

--- api/time.js
+++ api/time.js
@@ -1,5 +1,8 @@
-module.exports = function handler(req, res) {
+module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
   res.status(200).json({
     datetime: new Date().toLocaleString("en-CA", { timeZone: "America/Toronto" }),
     timezone: "America/Toronto"
